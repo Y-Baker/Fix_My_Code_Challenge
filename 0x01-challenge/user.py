b1d01dd6edc8e7c8a1c99459c1336ff3d4bc25ec
Yousef <yuossefbakier@gmail.com>
2023-10-18 02:21:54 +0300

    Fix code Style user

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-""" 
+"""
 User class
 """
 
+
 class User():
-    """ Documentation """
+    """ Define Class User """
 
     def __init__(self):
-        """ Documentation """
+        """ Initialize the class """
         self.__email = None
+
+    @property
+    def email(self):
+        """ Getter of email """
+        return self.__email
 
     @email.setter
     def email(self, value):
-        """ Documentation """
+        """ setter of email """
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-   
-    
+
 if __name__ == "__main__":
 
     u = User()
